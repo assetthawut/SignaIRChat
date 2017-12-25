f309d3e408f41d3ab0f494e9fafd8ac3e3e802a5
Sukmuang Setthawut - Together TH <s.sukmuang@togetherteam.co.th>
2017-12-25 09:33:01 +0700

    signalr

---
+++ SignaIRChat/ChatHub.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Collections;
+
+namespace SignaIRChat
+{
+    public class ChatHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+        }
+
+        public void NewMessage(string name,string message) {
+            Clients.All.addNewMessageToPage(name, message);   
+        }
+
+        public void test(string name, string message) {
+            Clients.All.jook(name, message);
+        }
+
+        public void test2(string name,string to, string message)
+        {
+             Clients.All.jook2(name, to,message);
+           // Clients.Client(Context.ConnectionId).jook2(name, message);
+        }
+
+
+        public void countnumber(int number) {
+            Clients.All.counter(number);
+        }
+
+
+
+    }
+}

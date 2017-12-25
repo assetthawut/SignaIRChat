0586e94e345b7416543e9369b823e2b635d9e886
Sukmuang Setthawut - Together TH <s.sukmuang@togetherteam.co.th>
2017-12-25 16:05:11 +0700

    update Sample

--- SignaIRChat/ChatHub.cs
+++ SignaIRChat/ChatHub.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
 using System.Collections;
+using System.Threading.Tasks;
 
 namespace SignaIRChat
 {
@@ -20,18 +21,18 @@ namespace SignaIRChat
             Clients.All.broadcastMessage(name, message);
         }
 
-        public void NewMessage(string name,string message) {
-            Clients.All.addNewMessageToPage(name, message);   
+        public void NewMessage(string name, string message) {
+            Clients.All.addNewMessageToPage(name, message);
         }
 
         public void test(string name, string message) {
             Clients.All.jook(name, message);
         }
 
-        public void test2(string name,string to, string message)
+        public void test2(string name, string to, string message)
         {
-             Clients.All.jook2(name, to,message);
-           // Clients.Client(Context.ConnectionId).jook2(name, message);
+            Clients.All.jook2(name, to, message);
+            // Clients.Client(Context.ConnectionId).jook2(name, message);
         }
 
 
@@ -39,6 +40,51 @@ namespace SignaIRChat
             Clients.All.counter(number);
         }
 
+
+        /*
+        public async Task<string> DoLongRunningThing(int progress) {
+            for (int i = 0; i <= 100; i++) {
+                   await Task.Delay(1);
+            }
+            return "Job done";
+        }
+        */
+
+        public string DoLongRunningThing(int progress) {
+            for (int i = 0; i <= 100; i++)
+            {
+
+            }
+            return "Short Run";
+        }
+
+        /*
+        public class username {
+
+            public string Username { get; set; }
+            public string Lastname { get; set; }
+        }
+
+        public void SendMessage(string name, string message) {
+            Clients.All.addContosoChatMessageToPage(new username() {
+                Username = "Joe",
+                Lastname = "Sett"
+            });
+        }
+        */
+
+        
+
+        public class ContosoChatMessage
+        {
+            public string UserName { get; set; }
+            public string Message { get; set; }
+        }
+
+        public void SendMessage(string name, string message)
+        {
+            Clients.All.addContosoChatMessageToPage(new ContosoChatMessage() { UserName = name, Message = message });
+        }
 
 
     }
